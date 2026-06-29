c8936fb3de6451d03f54dc44c43ad8d9d167dbe6
Huda <s75871@ocean.umt.edu.my>
2026-06-29 10:35:33 +0800

    update folder

--- src/java/com/dao/DBConnection.java
+++ src/java/com/dao/DBConnection.java
@@ -11,6 +11,7 @@ public class DBConnection {
         String url, user, password;
 
         if ("CLOUD".equals(env)) {
+            // Setup for Render / Production
             url = System.getenv("DB_URL");
             user = System.getenv("DB_USER");
             password = System.getenv("DB_PASS");
@@ -19,7 +20,13 @@ public class DBConnection {
             if (url == null || user == null || password == null) {
                 throw new SQLException("Environment variables tidak dijumpai. Sila semak setting Render!");
             }
+        } else if ("INFINITY".equals(env)) {
+            // Setup for InfinityFree
+            url = "jdbc:mysql://sql205.infinityfree.com:3306/if0_42293119_univents_db";
+            user = "if0_42293119";
+            password = "0Dmin123";
         } else {
+            // Fallback setup for Localhost (XAMPP/MAMP)
             url = "jdbc:mysql://localhost:3306/univents_db";
             user = "root";
             password = "admin";
